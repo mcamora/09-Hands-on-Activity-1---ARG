57628e4504b91136d0f63628975b94e7424d8060
Mc Nel Amora <amora.373352@pasay-edsa.sti.edu.ph>
2024-12-19 20:23:41 +0800

    Delete Rhombus.java

--- Rhombus.java
+++
@@ -1,9 +0,0 @@
-public class Rhombus extends Parallelogram{
-    public Rhombus() {
-        super();
-    }
-    public void showDescription(){
-        super.showDescription();
-        System.out.println("- has four congruent sides.");
-    }
-}
